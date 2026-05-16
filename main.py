654606c54dabcdd6c49cf44b992f8fe63324e479
Rafael Mello <almeidamellorafael@gmail.com>
2026-05-15 22:10:58 -0300

    add - primeiros exercícios

---
+++ main.py
@@ -0,0 +1,32 @@
+## Exercício 1
+
+#for i in range(1,11):
+#    print(i)
+
+## Exercício 2
+
+#for i in range(0,21,2):
+#    print(i)
+
+## Exercício 3
+
+#numero = 0;
+
+#for i in range(1,101):
+#    numero = numero + i
+
+#print(numero)
+
+## Exercício 4
+
+#total = 0
+
+#for i in range(1,6):
+#    nota = float(input("Digite sua nota: "))
+#    total += nota
+
+#media = total / 5
+#print(f"A média é {media}")
+
+## Exercício 5
+
