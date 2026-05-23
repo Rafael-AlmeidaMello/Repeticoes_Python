47ad106a886d69b106d67fe5e17a7ee1af5b3fb2
Rafael Mello <almeidamellorafael@gmail.com>
2026-05-22 22:08:08 -0300

    add - finalização de exercícios

--- main.py
+++ main.py
@@ -30,3 +30,82 @@
 
 ## Exercício 5
 
+#quantidade = int(input("quantos números você irá digitar? "))
+#i = 0
+#positivos = 0
+
+#while i < quantidade:
+#    numero = float(input("digite o número: "))
+#    if numero > 0:
+#        positivos = positivos + 1
+#    i = i + 1
+
+#print(positivos)
+
+## Exercício 6
+
+#tabuada = int(input("digite qual tabuada você deseja: "))
+
+#for i in range(1, 11):
+#    print(tabuada * i)
+
+## Exercício 7
+
+#numero = int(input("digite um número: "))
+#fatorial = 1
+
+#for i in range(numero, 1, -1):
+#    fatorial *= i
+    
+#print("O resultado é ", fatorial)
+
+## Exercício 8
+
+#quantidade = int(input("Digite a quantidade de termos: "))
+
+#a, b = 0, 1
+
+#print("Sequência de Fibonacci:")
+
+#for i in range(quantidade):
+#    print(a, end=" ")
+#    a, b = b, a + b
+
+## Exercício 9
+
+#senha = 123
+#password = int(input("digite a senha: "))
+
+#while password != senha:
+#    password = int(input("senha incorreta, tente novamente: "))
+
+#print("Login efetuado")
+
+## Exercício 10
+
+opcao = 0
+
+while opcao != 4:
+    print("MENU")
+    print("1 - Olá")
+    print("2 - Neymar")
+    print("3 - Mostrar mensagem")
+    print("4 - Sair")
+
+    opcao = int(input("Escolha uma opção: "))
+
+    match opcao:
+        case 1:
+            print("Olá, usuário!")
+
+        case 2:
+            print("Neymar Júnior")
+
+        case 3:
+            print("Python é muito legal!")
+
+        case 4:
+            print("Você saiu")
+
+        case _:
+            print("Opção inválida!")
